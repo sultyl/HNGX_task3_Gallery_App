d621ed090632051124cf1e3af1d778557567018d
Sultan Adeleke <70979325+sultyl@users.noreply.github.com>
2023-09-20 11:32:47 +0100

    Update LoginModal.js

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { signIn } from 'next-auth/react';
+import Link from 'next/link';
 
 Modal.setAppElement('#__next'); // Setting the root element for the modal
 
@@ -19,7 +20,7 @@ const LoginModal = ({ isOpen, onRequestClose }) => {
     <div className={`modal ${isOpen ? 'modal-open' : ''}`}>
       <div className="modal-content">
         <h2>Only Authenticated Users can use drag and drop function</h2>
-        <button onClick={handleLogin} className='bg-black text-white rounded-lg py-1 px-2 mt-3'>Login</button>
+        <Link href="https://kinggallery.vercel.app/Login" className='bg-black text-white rounded-lg py-1 px-2 mt-3'>Login</Link>
         </div>
     </div>
   );
