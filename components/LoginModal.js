37c62ae43dc73222bb304368cd498e6615f376c3
Sultan Adeleke <70979325+sultyl@users.noreply.github.com>
2023-09-20 15:22:17 +0100

    Update LoginModal.js

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -10,7 +10,7 @@ const LoginModal = ({ isOpen, onRequestClose }) => {
     <div className={`modal ${isOpen ? 'modal-open' : ''}`}>
       <div className="modal-content">
         <h2>Only Authenticated Users can use drag and drop function</h2>
-        <Link href="https://kinggallery.vercel.app/Login" className='bg-black text-white rounded-lg py-1 px-2 mt-3'>Login</Link>
+        <Link href="https://kinggallery.vercel.app/" className='bg-black text-white rounded-lg py-1 px-2 mt-3'>Login</Link>
         </div>
     </div>
   );
